6ea04b3d88c8a4a7081157aa1b2e25c095224889
DevStar999 <wolverinerage99@gmail.com>
2022-10-16 11:59:22 +0530

    Integrated AdMob to the project

--- app/src/main/java/com/nerdcoredevelopment/inappbillingdemo/IntroActivity.java
+++ app/src/main/java/com/nerdcoredevelopment/inappbillingdemo/IntroActivity.java
@@ -13,6 +13,12 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.google.android.gms.ads.MobileAds;
+
+/* TODO -> !! Caution !! - Always make sure to use a testing id for debugging and
+           testing purposes. Also ensure to use the Actual id just before creating a
+           release AAB or APK as then we want to have an actual Admob App Id
+*/
 public class IntroActivity extends AppCompatActivity {
 
     @SuppressLint("SourceLockedOrientationActivity")
@@ -33,6 +39,8 @@ public class IntroActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        MobileAds.initialize(this);
 
         AppCompatImageView developerLogoImageView = findViewById(R.id.developer_logo_image_view);
         developerLogoImageView.animate().alpha(1).setDuration(2000).start();
