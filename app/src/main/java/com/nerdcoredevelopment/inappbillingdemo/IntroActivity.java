68f3b0e87a6828cbc2e84088865f741f934138be
DevStar999 <wolverinerage99@gmail.com>
2022-11-14 11:44:07 +0530

    Completed the implementation of App Open Ad

--- app/src/main/java/com/nerdcoredevelopment/inappbillingdemo/IntroActivity.java
+++ app/src/main/java/com/nerdcoredevelopment/inappbillingdemo/IntroActivity.java
@@ -1,19 +1,21 @@
 package com.nerdcoredevelopment.inappbillingdemo;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
-import androidx.appcompat.widget.AppCompatImageView;
-
 import android.annotation.SuppressLint;
+import android.app.Application;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-import com.google.android.gms.ads.MobileAds;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.AppCompatImageView;
+
+import com.nerdcoredevelopment.inappbillingdemo.MyApplication.OnShowAdCompleteListener;
 
 /* TODO -> !! Caution !! - Always make sure to use a testing id for debugging and
            testing purposes. Also ensure to use the Actual id just before creating a
@@ -40,19 +42,38 @@ public class IntroActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
-        MobileAds.initialize(this);
-
         AppCompatImageView developerLogoImageView = findViewById(R.id.developer_logo_image_view);
         developerLogoImageView.animate().alpha(1).setDuration(2000).start();
 
-        new Handler().postDelayed(new Runnable() {
+        new CountDownTimer(3000, 10000) {
             @Override
-            public void run() {
-                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-                startActivity(intent);
-                finish();
+            public void onTick(long millisUntilFinished) {}
+            @Override
+            public void onFinish() {
+                Application application = getApplication();
+
+                // If the application is not an instance of MyApplication, log an error message and
+                // start the MainActivity without showing the app open ad.
+                if (!(application instanceof MyApplication)) {
+                    startMainActivity();
+                    return;
+                }
+
+                // Show the app open ad.
+                ((MyApplication) application).showAdIfAvailable(IntroActivity.this, new OnShowAdCompleteListener() {
+                    @Override
+                    public void onShowAdComplete() {
+                        startMainActivity();
+                    }
+                });
             }
-        }, 3000);
+        }.start();
+    }
+
+    private void startMainActivity() {
+        Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(intent);
+        finish();
     }
 }
