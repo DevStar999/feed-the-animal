4bf85574885df5255618d60a5e8c05d2973ce64d
DevStar999 <wolverinerage99@gmail.com>
2022-12-20 01:55:47 +0530

    Added a TODO related to ads

--- app/src/main/java/com/nerdcoredevelopment/inappbillingdemo/MainActivity.java
+++ app/src/main/java/com/nerdcoredevelopment/inappbillingdemo/MainActivity.java
@@ -88,6 +88,7 @@ import java.util.List;
            'Order Ids' and/or 'Purchase Tokens' by using methods of queryPurchaseHistoryAsync() or queryPurchaseAsync() from
            original Google Billing Library
 */
+// TODO -> Make code changes to make ads suitable for children and families and prohibit display of sensitive ads
 public class MainActivity extends AppCompatActivity implements
         InfoFragment.OnInfoFragmentInteractionListener,
         NavigationFragment.OnNavigationFragmentInteractionListener,
