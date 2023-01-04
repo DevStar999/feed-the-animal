f41fd9e692bfa8b081f371260574c76db160b1fd
DevStar999 <wolverinerage99@gmail.com>
2023-01-04 12:43:41 +0530

    Updated the internet connection checking method as per the results of experiment done in a practise project

--- app/src/main/java/com/nerdcoredevelopment/inappbillingdemo/MainActivity.java
+++ app/src/main/java/com/nerdcoredevelopment/inappbillingdemo/MainActivity.java
@@ -318,31 +318,21 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     private boolean isInternetConnected() {
-        /* TODO -> We may need a better tested out method to correctly check if device is connected to a network and has
-                   internet. We can use the following reference -> "stackoverflow.com/questions/5474089/" +
-                   + "how-to-check-currently-internet-connection-is-available-or-not-in-android"
-        */
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        if (cm == null) {
-            return false;
-        }
+        if (cm == null) { return false; }
         /* NetworkInfo is deprecated in API 29 so we have to check separately for higher API Levels */
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             Network network = cm.getActiveNetwork();
-            if (network == null) {
-                return false;
-            }
+            if (network == null) { return false; }
             NetworkCapabilities networkCapabilities = cm.getNetworkCapabilities(network);
-            if (networkCapabilities == null) {
-                return false;
-            }
+            if (networkCapabilities == null) { return false; }
             boolean isInternetSuspended = !networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_NOT_SUSPENDED);
             return (networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
                     && networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
                     && !isInternetSuspended);
         } else {
             NetworkInfo networkInfo = cm.getActiveNetworkInfo();
-            return networkInfo != null && networkInfo.isConnected();
+            return (networkInfo != null && networkInfo.isConnected());
         }
     }
 
